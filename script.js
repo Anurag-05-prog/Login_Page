a53f254efb360a7e2a6e3c41da0e0239e3b764ed
anurag singh <anuragsingh6501@gmail.com>
2024-03-12 18:24:18 +0530

    YAC

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const darkModeSwitch = document.querySelector('.dark-mode');
-const lightModeSwitch = document.querySelector('.light-mode');
-const currTheme = localStorage.getItem('theme');
-
 function moveElements() {
     anime({
         targets: '.square, .circle, .triangle',
@@ -28,28 +24,3 @@ function moveElements() {
     });
 }
 moveElements();
-
-darkModeSwitch.addEventListener('click', function() {
-    lightModeSwitch.style.display = 'block';
-    darkModeSwitch.style.display = 'none';
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-});
-
-lightModeSwitch.addEventListener('click', function() {
-    lightModeSwitch.style.display = 'none';
-    darkModeSwitch.style.display = 'block';
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-});
-
-if(currTheme) {
-    if(currTheme === 'light') {
-        lightModeSwitch.style.display = 'block';
-        darkModeSwitch.style.display = 'none';
-    } else {
-        lightModeSwitch.style.display = 'none';
-        darkModeSwitch.style.display = 'block';
-    }
-    document.documentElement.setAttribute('data-theme', currTheme);
-}
