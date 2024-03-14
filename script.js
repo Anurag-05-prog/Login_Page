f3ba92532e381a16124ae0296be0ba47b52e7da9
anurag singh <anuragsingh6501@gmail.com>
2024-03-15 00:01:13 +0530

    UI Update

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 "use strict";
 
 function moveElements() {
-    anime({
-        targets: '.square, .circle, .triangle',
-        translateX: function() {
-            return anime.random(-800, 800);
-        },
-        translateY: function() {
-            return anime.random(-500, 500);
-        },
-        translateZ: function() {
-            return anime.random(-100, 100);
-        },
-        rotate: function() {
-            return anime.random(0, 360);
-        },
-        scale: function() {
-            return anime.random(.2, 2);
-        },
-        duration: 1000,
-        easing: 'easeInOutQuad',
-        complete: moveElements,
-    });
+  anime({
+    targets: ".square, .circle, .triangle",
+    translateX: function () {
+      return anime.random(-800, 800);
+    },
+    translateY: function () {
+      return anime.random(-500, 500);
+    },
+    translateZ: function () {
+      return anime.random(-50, 50);
+    },
+    rotate: function () {
+      return anime.random(0, 360);
+    },
+    scale: function () {
+      return anime.random(.2, .5);
+    },
+    duration: 1000,
+    easing: "easeInOutQuad",
+    complete: moveElements,
+  });
 }
 moveElements();
